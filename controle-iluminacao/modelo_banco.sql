e8abd065d0684dbd0f9459e7a20c56e27e37f801
Heitor Novais <hnovais95@gmail.com>
2020-05-03 18:42:19 -0300

    Adiciona projeto de controle de iluminação

---
+++ controle-iluminacao/modelo_banco.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE lap; 
+
+CREATE TABLE luminarias
+(
+	id int NOT NULL,
+	status int NOT NULL DEFAULT 0,
+	pot int NOT NULL DEFAULT 0
+);
+
+INSERT INTO luminarias(id, status, pot) VALUES(1, 0, 0);
+INSERT INTO luminarias(id, status, pot) VALUES(2, 0, 0);
+INSERT INTO luminarias(id, status, pot) VALUES(3, 0, 0);
